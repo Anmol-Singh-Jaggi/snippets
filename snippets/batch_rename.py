4bd84882ea2997783c893373796ab6881a574c02
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-03-11 03:05:16 +0530

    Maintenance

--- snippets/batch_rename.py
+++ snippets/batch_rename.py
@@ -1,12 +1,12 @@
 from os import listdir, rename
-from os.path import isfile, join, getmtime
+from os.path import isfile, join, getmtime, basename
 import sys
 
 
 def process_file_name(file_name, *args):
     sorted_file_name_list = args[0]
     file_order = sorted_file_name_list.index(file_name)
-    new_file_name = str(file_order + 1).rjust(2, '0')
+    new_file_name = str(file_order + 1).rjust(3, '0')
     return new_file_name
 
 
@@ -16,18 +16,16 @@ def main():
     # Get the list of files in the input directory
     file_name_list = [file_name for file_name in listdir(
         input_directory_path) if isfile(join(input_directory_path, file_name))]
-    file_name_list.remove(sys.argv[0])
+    file_name_list.remove(basename(sys.argv[0]))
     print file_name_list, "\n\n"
 
     # Sort according to file-creation time
     sorted_file_name_list = sorted(file_name_list, key=getmtime)
     print sorted_file_name_list, "\n\n\n"
 
-    index = 1
-    for file_name in file_name_list:
+    for index, file_name in enumerate(file_name_list):
         new_file_name = process_file_name(file_name, sorted_file_name_list)
-        print str(index) + ") " + file_name + " --> " + new_file_name
-        index += 1
+        print str(index + 1) + ") '" + file_name + "'' --> '" + new_file_name + "'"
         if isfile(join(input_directory_path, new_file_name)):
             print "Error: Target name exists already!!"
             continue
