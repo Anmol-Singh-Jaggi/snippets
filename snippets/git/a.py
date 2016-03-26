d34125fec75161f031319613196cb8e5440d584d
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-03-27 02:21:22 +0530

    Added git/

---
+++ snippets/git/a.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import os
+
+
+command = r'ls -d */ | cut --field=1 > dirs.temp'
+os.system(command)
+dirs = open('dirs.temp').readlines()
+
+for i in dirs:
+    i = "'" + i.strip() + "'"
+    command = 'cd ' + i + ' && git status'
+    print(command)
+    os.system(command)
