1c7f994e847b227b1bc9d1f1fbdca46545f77e4e
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-05-15 01:38:07 +0530

    Maintenance

--- snippets/hosts/src/hosts.py
+++ snippets/hosts/src/hosts.py
@@ -1,27 +1,24 @@
-block_list = []
-block_list.append('facebook')
-block_list.append('fb')
-block_list.append('linkedin')
-block_list.append('github')
-'''
-block_list.append('stackoverflow')
-block_list.append('youtube')
-block_list.append('gmail')
-block_list.append('quora')
-'''
+#!/usr/bin/env python3
+from hosts_block_list import block_list
 
-block_list_processed = []
-for i in block_list:
-    i += ".com"
-    block_list_processed.append(i)
-    block_list_processed.append("www." + i)
 
-block_list_string = "\n\n0.0.0.0"
-# print block_string
+def main():
+    block_list_processed = []
+    for i in block_list:
+        i += ".com"
+        block_list_processed.append(i)
+        block_list_processed.append("www." + i)
 
-for i in block_list_processed:
-    block_list_string += " " + i
+    block_list_string = "\n\n0.0.0.0"
+    # print(block_string)
 
-block_list_string += "\n\n"
+    for i in block_list_processed:
+        block_list_string += " " + i
 
-print block_list_string
+    block_list_string += "\n\n"
+
+    print(block_list_string)
+
+
+if __name__ == '__main__':
+    main()
