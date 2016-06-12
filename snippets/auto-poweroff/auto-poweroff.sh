5a57fd45d9c08e297cbbb61f34f100e1ba62db4f
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-06-12 16:23:06 +0530

    Added auto-poweroff

--- snippets/auto-poweroff/auto-poweroff.sh
+++ snippets/auto-poweroff/auto-poweroff.sh
@@ -18,9 +18,12 @@ fi
 # Source the environment variables required for notify-send to work.
 . /home/anmol/.env_vars
 
+# Percentage at which to show low-battery notification
 low_notif_percentage=20
-critical_notif_percentage=100
-critical_action_percentage=95
+# Percentage at which to show critical-battery notification
+critical_notif_percentage=15
+# Percentage at which to power-off
+critical_action_percentage=10
 
 
 if [ "${level}" -le ${critical_action_percentage} ]; then
