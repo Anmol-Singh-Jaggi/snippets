1c7559dc57c9091fdbadd9ba7a306181c691a53c
Anmol Singh Jaggi <anmolj.65@gmail.com>
2017-06-19 21:16:43 +0530

    Added grocery_bill_split.py

---
+++ snippets/grocery_bill_split.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+def main():
+    entity_vs_amount = {}
+    while True:
+        # A line is of the format `num1 num2 num3 ... label-string`,
+        # denoting that the amounts (num1 + num2 + num3) has to be
+        # divided between the persons mentioned in the labels.
+        # For example, `12 34 55 AS` => 101 has to be divided between 'A' and
+        # 'S'.
+        line = input()
+        if not len(line):
+            # Break on empty line.
+            break
+        line = line.split()
+        amounts = line[:-1]
+        entities = line[-1]
+        amount = sum(float(num) for num in amounts)
+        amount_per_entity = float(amount) / len(entities)
+        for entity in entities:
+            entity_amount = entity_vs_amount.get(entity, 0)
+            entity_amount += amount_per_entity
+            entity_vs_amount[entity] = entity_amount
+    print(entity_vs_amount)
+
+
+if __name__ == '__main__':
+    main()
