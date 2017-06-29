2144b04bb14a06cbd8ba587a56d0860f3538ccd4
Anmol Singh Jaggi <anmolj.65@gmail.com>
2017-06-29 21:45:41 +0530

    Added feature to take input from file

--- snippets/grocery_bill_split.py
+++ snippets/grocery_bill_split.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
+import sys
+import traceback
 
 
-def main():
+def process(lines):
     entity_vs_amount = {}
-    while True:
+    for line in lines:
         # A line is of the format `num1 num2 num3 ... label-string`,
         # denoting that the amounts (num1 + num2 + num3) has to be
         # divided between the persons mentioned in the labels.
         # For example, `12 34 55 AS` => 101 has to be divided between 'A' and
         # 'S'.
-        line = input()
-        if not len(line):
-            # Break on empty line.
-            break
         line = line.split()
         assert(len(line) > 1)
         amounts = line[:-1]
@@ -23,9 +21,36 @@ def main():
             entity_amount = entity_vs_amount.get(entity, 0)
             entity_amount += amount_per_entity
             entity_vs_amount[entity] = entity_amount
-    print(entity_vs_amount)
-    total_amount = sum(entity_vs_amount.values())
-    print("Total amount = {}".format(total_amount))
+    return entity_vs_amount
+
+
+def get_input_from_stdin():
+    lines = []
+    while True:
+        line = input()
+        if not len(line):
+            # Break on empty line.
+            break
+        lines.append(line)
+    return lines
+
+
+def main():
+    if len(sys.argv) > 1:
+        input_file_path = sys.argv[1]
+        with open(input_file_path) as input_file:
+            lines = input_file.readlines()
+    else:
+        lines = get_input_from_stdin()
+    try:
+        entity_vs_amount = process(lines)
+        print(entity_vs_amount)
+        total_amount = sum(entity_vs_amount.values())
+        print("Total amount = {}".format(total_amount))
+    except Exception:
+        exception_traceback = traceback.format_exc()
+        print("Error while processing input:\n\n---- start ----\n{}\n---- end ----\n\nwith the following exception trace:\n{}".format(
+            lines, exception_traceback))
 
 
 if __name__ == '__main__':
